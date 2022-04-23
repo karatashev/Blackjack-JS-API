7dc9337e2fff4a518305f17e080f9932c7de56e3
Vasil Karatashev <karatashev@gmail.com>
2022-04-23 03:04:35 +0200

    fixed flipping dealers first card every time when player lost

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //DOM
 let deckId = ''; //we store our deck in global variable
-let playerCardOne, playerCardTwo, dealerClosedCard, dealerCardTwo, sumPlayerCards;
+let playerCardOne, playerCardTwo, dealerClosedCard, dealerCardTwo, totalDealerResult, sumPlayerCards;
 
 //first card of the dealer
 let dealerCardOne = document.querySelector('.dealer-card1');
@@ -13,7 +13,6 @@ const playerScore = document.querySelector('.player-score');
 
 const hitButton = document.querySelector('.hit');
 let addPlayerCards = [];
-
 let sumDealersCards = [];
 
 
@@ -33,7 +32,7 @@ fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6') //6 d
 document.querySelector('.deal').addEventListener('click', dealCards)
 
 function dealCards() {
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`
   //we are drawing 4 cards from the deck that we store his ID, 2 for the player, 2 for the dealer
   fetch(url)
     .then(res => res.json()) //parse response as JSON
@@ -44,23 +43,22 @@ function dealCards() {
       //images for the Cards
       document.querySelector('.player-card1').src = data.cards[0].image;
       // Dealer first card is hidden from the start!
-      dealerCardOne.src = data.cards[1].image;
      
       dealerCardOne.src = 'https://opengameart.org/sites/default/files/card%20back%20red.png';
-      document.querySelector('.player-card2').src = data.cards[2].image;
-      document.querySelector('.dealer-card2').src = data.cards[3].image;
+      document.querySelector('.player-card2').src = data.cards[1].image;
+      document.querySelector('.dealer-card2').src = data.cards[2].image;
       //Starting Players cards
       playerCardOne = convertToNum(data.cards[0].value);
-      playerCardTwo = convertToNum(data.cards[2].value);
+      playerCardTwo = convertToNum(data.cards[1].value);
       addPlayerCards.push(playerCardOne, playerCardTwo);
       console.log(addPlayerCards);
       
       //Starting Dealers cards
-      dealerCardOne = convertToNum(data.cards[1].value);
-      dealerCardTwo = convertToNum(data.cards[3].value);
+      // dealerCardOne = convertToNum(data.cards[1].value);
+      dealerCardTwo = convertToNum(data.cards[2].value);
 
 
-      totalDealerResult = dealerCardTwo + dealerCardOne;
+      totalDealerResult = dealerCardTwo;
       dealerScore.innerText = dealerCardTwo;
  
       //sum of the first two cards
@@ -90,14 +88,15 @@ hitButton.addEventListener('click', hitAction)
 function hitAction() {
   console.log('hit me');
 
+  //To give player additional card if he don't have blackjack 
  if (sumPlayerCards <= 20) {
-
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`) // 1 card for hit
     .then(res => res.json()) //parse response as JSON
     .then(data => {
       console.log(data)
       deckId = data.deck_id;
 
+    //Create additional card whenever conditions are met
     const playerCards = document.querySelector('.player-cards');
     let cardElement = document.createElement('img');
     playerCards.appendChild(cardElement);
@@ -115,10 +114,25 @@ function hitAction() {
     playerScore.innerText = sumPlayerCards;
     console.log(sumPlayerCards)
 
+    //To flip the closed dealer card and sum dealer result because player lost
     if (sumPlayerCards > 21) {
-      // dealerCardOne.src = data.cards[1].image;
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`) //for closed card
+    .then(res => res.json()) //parse response as JSON
+    .then(data => {
+      console.log(data)
+      deckId = data.deck_id;
+      dealerCardOne.src = data.cards[0].image;
+      dealerCardOne = convertToNum(data.cards[0].value);
+
+      totalDealerResult = dealerCardTwo + dealerCardOne;
       dealerScore.innerText = totalDealerResult;
-    }
+
+      checkForWinner();
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+  }
     
   
     // checkForWinner();
@@ -130,21 +144,6 @@ function hitAction() {
     }
   }
 
-
-  
-  // document.querySelector('.player-card3').src = data.cards[0].image;
-  // playerCardThree = convertToNum(data.cards[0].value);
-  // totalPlayerResult = playerCardOne + playerCardTwo + playerCardThree;
-  // playerScore.innerText = totalPlayerResult;
-
-
-
-  // if (totalPlayerResult <= 20) {
-  //   document.querySelector('.player-card4').src = data.cards[1].image;
-  //   playerCardFour = convertToNum(data.cards[1].value);
-  //   totalPlayerResult = playerCardOne + playerCardTwo + playerCardThree + playerCardFour;
-  //   playerScore.innerText = totalPlayerResult;
-  // }
 
 
 //To convert the cards with string Values
@@ -160,6 +159,7 @@ function convertToNum(val) {
   }
 }
 
+//Checks who won
 function checkForWinner() {
   if (sumPlayerCards > totalDealerResult && sumPlayerCards < 21) {
     result.innerText = ('PLAYER WINS');
