e84a409c27e92eb73ac6bf5f1e07b1f082286d18
Vasil Karatashev <karatashev@gmail.com>
2022-05-01 04:08:57 +0200

    improvements on stand action button

--- script.js
+++ script.js
@@ -1,187 +1,246 @@
 //DOM
-let deckId = ''; //we store our deck in global variable
-let playerCardOne, playerCardTwo, dealerClosedCard, dealerCardTwo, totalDealerResult, sumPlayerCards;
+let deckId = ""; //we store our deck in global variable
+let playerCardOne,
+  playerCardTwo,
+  dealerClosedCard,
+  dealerCardTwo,
+  sumPlayerCards,
+  sumDealersCards;
 
 //first card of the dealer
-let dealerCardOne = document.querySelector('.dealer-card1');
+let dealerCardOne = document.querySelector(".dealer-card1");
 
+let result = document.querySelector(".result");
 
-let result = document.querySelector('.result');
+const dealerScore = document.querySelector(".dealer-score");
+const playerScore = document.querySelector(".player-score");
 
-const dealerScore = document.querySelector('.dealer-score');
-const playerScore = document.querySelector('.player-score');
+//Buttons
+const dealButton = document.querySelector(".deal");
+const hitButton = document.querySelector(".hit");
+const standButton = document.querySelector(".stand");
 
-const hitButton = document.querySelector('.hit');
+//we store value from the cards here
 let addPlayerCards = [];
-let sumDealersCards = [];
+let addDealersCards = [];
 
+fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6") //6 decks for blackjack
+  .then((res) => res.json()) //parse response as JSON
+  .then((data) => {
+    console.log(data);
+    deckId = data.deck_id;
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
 
-fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6') //6 decks for blackjack
-.then(res => res.json()) //parse response as JSON
-.then(data => {
-  console.log(data)
-  deckId = data.deck_id;
-  
-})
-.catch(err => {
-  console.log(`error ${err}`)
-});
-
-// document.querySelector('.stand').addEventListener('click', standAction)
-
-document.querySelector('.deal').addEventListener('click', dealCards)
+dealButton.addEventListener("click", dealCards);
 
 function dealCards() {
-  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`
+  const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`;
   //we are drawing 4 cards from the deck that we store his ID, 2 for the player, 2 for the dealer
   fetch(url)
-    .then(res => res.json()) //parse response as JSON
-    .then(data => {
-      console.log(data)
-      console.log(data.remaining)
-      // let addPlayerCards = [];
+    .then((res) => res.json()) //parse response as JSON
+    .then((data) => {
+      console.log(data);
+      console.log(data.remaining);
       //images for the Cards
-      document.querySelector('.player-card1').src = data.cards[0].image;
+      document.querySelector(".player-card1").src = data.cards[0].image;
       // Dealer first card is hidden from the start!
-     
-      dealerCardOne.src = 'https://opengameart.org/sites/default/files/card%20back%20red.png';
-      document.querySelector('.player-card2').src = data.cards[1].image;
-      document.querySelector('.dealer-card2').src = data.cards[2].image;
+
+      dealerCardOne.src =
+        "https://opengameart.org/sites/default/files/card%20back%20red.png";
+      document.querySelector(".player-card2").src = data.cards[1].image;
+      document.querySelector(".dealer-card2").src = data.cards[2].image;
       //Starting Players cards
       playerCardOne = convertToNum(data.cards[0].value);
       playerCardTwo = convertToNum(data.cards[1].value);
       addPlayerCards.push(playerCardOne, playerCardTwo);
       console.log(addPlayerCards);
-      
+
       //Starting Dealers cards
-      // dealerCardOne = convertToNum(data.cards[1].value);
       dealerCardTwo = convertToNum(data.cards[2].value);
 
+      //push value to the dealers array
+      addDealersCards.push(dealerCardTwo);
+      console.log(addDealersCards);
 
-      totalDealerResult = dealerCardTwo;
-      dealerScore.innerText = dealerCardTwo;
- 
-      //sum of the first two cards
+      //sum of the dealers cards
+      sumDealersCards = addDealersCards.reduce((prevCard, curCard) => {
+        return prevCard + curCard;
+      }, 0);
+
+      dealerScore.innerText = sumDealersCards;
+      console.log(sumDealersCards);
+
+      //sum of the players first two cards
       sumPlayerCards = addPlayerCards.reduce((prevCard, curCard) => {
         return prevCard + curCard;
       }, 0);
-    
+
       playerScore.innerText = sumPlayerCards;
-    
       console.log(sumPlayerCards);
-      
+
       //Check is player has drawn blackjack from the start
       if (sumPlayerCards === 21) {
-        result.innerText = ('BLACKJACK PLAYER WINS');
+        result.innerText = "BLACKJACK PLAYER WINS";
       }
-
     })
-    .catch(err => {
-      console.log(`error ${err}`)
+    .catch((err) => {
+      console.log(`error ${err}`);
     });
 }
 
-
 //HIT BUTTON ACTION
-hitButton.addEventListener('click', hitAction)
+hitButton.addEventListener("click", hitAction);
 
 function hitAction() {
-  console.log('hit me');
+  console.log("hit me");
 
-  //To give player additional card if he don't have blackjack 
- if (sumPlayerCards <= 20) {
+  //To give player additional card if he don't have blackjack
+  if (sumPlayerCards <= 20) {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`) // 1 card for hit
-    .then(res => res.json()) //parse response as JSON
-    .then(data => {
-      console.log(data)
-      deckId = data.deck_id;
+      .then((res) => res.json()) //parse response as JSON
+      .then((data) => {
+        console.log(data);
+        deckId = data.deck_id;
 
-    //Create additional card whenever conditions are met
-    const playerCards = document.querySelector('.player-cards');
-    let cardElement = document.createElement('img');
-    playerCards.appendChild(cardElement);
+        //Create additional card whenever conditions are met
+        const playerCards = document.querySelector(".player-cards");
+        let cardElement = document.createElement("img");
+        playerCards.appendChild(cardElement);
 
-    cardElement.src = data.cards[0].image;
-    cardElement.classList.add('.new-player-cards')
-    cardElement = convertToNum(data.cards[0].value);
-    addPlayerCards.push(cardElement);
-    console.log(addPlayerCards);
-    
-    sumPlayerCards = addPlayerCards.reduce((prevCard, curCard) => {
-      return prevCard + curCard;
-    }, 0);
+        cardElement.src = data.cards[0].image;
+        cardElement.classList.add(".new-player-cards");
+        cardElement = convertToNum(data.cards[0].value);
+        addPlayerCards.push(cardElement);
+        console.log(addPlayerCards);
 
-    playerScore.innerText = sumPlayerCards;
-    console.log(sumPlayerCards)
+        sumPlayerCards = addPlayerCards.reduce((prevCard, curCard) => {
+          return prevCard + curCard;
+        }, 0);
 
-    //To flip the closed dealer card and sum dealer result because player lost
-    if (sumPlayerCards > 21) {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`) //for closed card
-    .then(res => res.json()) //parse response as JSON
-    .then(data => {
-      console.log(data)
+        playerScore.innerText = sumPlayerCards;
+        console.log(sumPlayerCards);
+
+        //To flip the closed dealer card and sum dealer result because player lost
+        if (sumPlayerCards > 21) {
+          fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`) //for closed card
+            .then((res) => res.json()) //parse response as JSON
+            .then((data) => {
+              console.log(data);
+              deckId = data.deck_id;
+              dealerCardOne.src = data.cards[0].image;
+              dealerCardOne = convertToNum(data.cards[0].value);
+              addDealersCards.push(dealerCardOne);
+              console.log(addDealersCards);
+
+              sumDealersCards = addDealersCards.reduce((prevCard, curCard) => {
+                return prevCard + curCard;
+              }, 0);
+
+              dealerScore.innerText = sumDealersCards;
+
+              checkForWinner();
+            })
+            .catch((err) => {
+              console.log(`error ${err}`);
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(`error ${err}`);
+      });
+  }
+}
+
+//STAND BUTTON ACTION
+standButton.addEventListener("click", standAction);
+
+function standAction() {
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`) //for closed card
+    .then((res) => res.json()) //parse response as JSON
+    .then((data) => {
+      console.log(data);
       deckId = data.deck_id;
       dealerCardOne.src = data.cards[0].image;
       dealerCardOne = convertToNum(data.cards[0].value);
+      addDealersCards.push(dealerCardOne);
+      console.log(addDealersCards);
 
-      totalDealerResult = dealerCardTwo + dealerCardOne;
-      dealerScore.innerText = totalDealerResult;
+      sumDealersCards = addDealersCards.reduce((prevCard, curCard) => {
+        return prevCard + curCard;
+      }, 0);
+      console.log(sumDealersCards);
+      dealerScore.innerText = sumDealersCards;
 
-      checkForWinner();
+      if (sumDealersCards >= 17 && sumDealersCards < 21) {
+        result.innerText = "DEALER WINS";
+      }
+      else if (sumDealersCards < 17) {
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+          .then((res) => res.json()) //parse response as JSON
+          .then((data) => {
+            console.log(data);
+            deckId = data.deck_id;
+            const dealerCards = document.querySelector(".dealer-cards");
+            let cardElement = document.createElement("img");
+            dealerCards.appendChild(cardElement);
+
+            cardElement.src = data.cards[0].image;
+            cardElement.classList.add(".new-dealer-cards");
+            cardElement = convertToNum(data.cards[0].value);
+            addDealersCards.push(cardElement);
+            console.log(addDealersCards);
+
+            sumDealersCards = addDealersCards.reduce((prevCard, curCard) => {
+              return prevCard + curCard;
+            }, 0);
+
+            dealerScore.innerText = sumDealersCards;
+            console.log(sumDealersCards);
+
+            checkForWinner();
+          })
+          .catch((err) => {
+            console.log(`error ${err}`);
+          });
+      }
+      else {
+        result.innerText = "PLAYER WINS";
+      }
     })
-    .catch(err => {
-      console.log(`error ${err}`)
+    .catch((err) => {
+      console.log(`error ${err}`);
     });
-  }
-    
-  
-    // checkForWinner();
-  })
-  .catch(err => {
-    console.log(`error ${err}`)
-  });
-  
-    }
-  }
-
-
+}
 
 //To convert the cards with string Values
 function convertToNum(val) {
-  if (val === 'ACE') {
+  if (val === "ACE") {
     return 11;
-  }
-  else if (val === 'KING' || val === 'QUEEN' || val === 'JACK') {
+  } else if (val === "KING" || val === "QUEEN" || val === "JACK") {
     return 10;
-  }
-  else {
-    return Number(val)
+  } else {
+    return Number(val);
   }
 }
 
 //Checks who won
 function checkForWinner() {
-  if (sumPlayerCards > totalDealerResult && sumPlayerCards < 21) {
-    result.innerText = ('PLAYER WINS');
-  }
-  else if (totalDealerResult > 21 && sumPlayerCards <= 20) {
-    result.innerText = ('PLAYER WINS');
-  }
-  else if (sumPlayerCards === 21 && totalDealerResult < 21) {
-    result.innerText = ('BLACKJACK PLAYER WINS');
-  }
-  else if (totalDealerResult > sumPlayerCards && totalDealerResult < 21) {
-    result.innerText = ('DEALER WINS');
-  }
-  else if (sumPlayerCards > 21 && totalDealerResult <= 20) {
-    result.innerText = ('DEALER WINS');
-  }
-  else if (totalDealerResult === 21 && sumPlayerCards <= 20) {
-    result.innerText = ('BLACKJACK DEALER WINS');
-  }
-  else {
-    result.innerText = ('DRAW');
+  if (sumPlayerCards > sumDealersCards && sumPlayerCards < 21) {
+    result.innerText = "PLAYER WINS";
+  } else if (sumDealersCards > 21 && sumPlayerCards <= 20) {
+    result.innerText = "PLAYER WINS";
+  } else if (sumPlayerCards === 21 && sumDealersCards < 21) {
+    result.innerText = "BLACKJACK PLAYER WINS";
+  } else if (sumDealersCards > sumPlayerCards && sumDealersCards < 21) {
+    result.innerText = "DEALER WINS";
+  } else if (sumPlayerCards > 21 && sumDealersCards <= 20) {
+    result.innerText = "DEALER WINS";
+  } else if (sumDealersCards === 21 && sumPlayerCards <= 20) {
+    result.innerText = "BLACKJACK DEALER WINS";
+  } else {
+    result.innerText = "DRAW";
   }
 }
-
-
